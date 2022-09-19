d2230071d0cee120e9a6eb0ab502c6ee32afa05a
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-09-19 08:33:48 -0400

    worked on question 1b)

--- A1codes.py
+++ A1codes.py
@@ -1,4 +1,5 @@
-import numpy.linalg.solve as np
+import cvxopt
+import numpy as np
 import cvxopt.solvers as solvers
 import math
 from matplotlib import pyplot as plt
@@ -16,16 +17,61 @@ def minimizeL2(X, y):
 
 def minimizeL1(X, y):
     n = len(X)
+    d = len(X[0])
+    identityMatrixN = np.identity(n)
+    negativeIdentityMatrixN = np.negative(identityMatrixN)
+    negativeIdentityMatrixN = negativeIdentityMatrixN.astype(int)
+    emptyMatrixNxD = np.zeros((n, d)).astype(int)
+    negativeX = np.negative(X)
 
-    y_ = np.matmul(X, w)
-    fn = y_ - y
+    GFirstRow = np.concatenate((emptyMatrixNxD, negativeIdentityMatrixN), axis=1)
+    GSecondRow = np.concatenate((X, negativeIdentityMatrixN), axis=1)
+    GThirdRow = np.concatenate((negativeX, negativeIdentityMatrixN), axis=1)
 
-    sum = 0
-    for counter in fn:
-        for counter2 in counter:
-            sum = sum + math.fabs(counter2)
+    G = np.concatenate((GFirstRow, GSecondRow), axis=0)
+    G = np.concatenate((G, GThirdRow), axis=0)
+    G = G.astype(np.double)
+    print(G)
 
-    sum = sum / n
+    emptyVectorN = np.zeros((n, 1)).astype(int)
+    negativeY = np.negative(y)
+
+    h = np.concatenate((emptyVectorN, y), axis=0)
+    h = np.concatenate((h, negativeY), axis=0)
+
+    #h = np.maximum(h, h.transpose())
+    print(h)
+
+    c = matrix([-1.0, -1.0])
+
+    print(c)
+    print(3*n)
+
+    w = solvers.lp(c, G, h)
+    return w
+
+def minimizeLinf(X, y):
+    n = len(X)
+    d = len(X[0])
+
+from cvxopt import matrix, solvers
+
+A = matrix([[0.0, 0.0, 0.0, -1.0, 0.0], [0.0, 0.0, 0.0, 0.0, -1.0]])
+b = matrix([0.0, 0.0, 1.0, 1.0, 0.0])
+c = matrix([1.0, 1.0])
+
+print(A)
+
+sol=solvers.lp(c,A,b)
+print(sol['x'])
+
+X = np.array([[1,2,3],[4,5,6]])
+
+y = np.array([[3],[5]])
+
+w = minimizeL1(X, y)
+
+
 
 #def minimizeLinf(X, y):
 
