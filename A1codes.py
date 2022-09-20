f4f4b04cf41c310fad4019d8b1eaa823a8a2ec47
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-09-20 13:03:22 -0400

    worked on 1c)

--- A1codes.py
+++ A1codes.py
@@ -1,6 +1,7 @@
 import cvxopt
 import numpy as np
 import cvxopt.solvers as solvers
+from cvxopt import matrix;
 import math
 from matplotlib import pyplot as plt
 
@@ -10,6 +11,8 @@ y = np.array([[6], [13], [23]])
 
 def minimizeL2(X, y):
     XInverse = np.linalg.inv(X);
+
+    print(XInverse)
 
     w = XInverse * y
 
@@ -31,28 +34,22 @@ def minimizeL1(X, y):
     G = np.concatenate((GFirstRow, GSecondRow), axis=0)
     G = np.concatenate((G, GThirdRow), axis=0)
     G = G.astype(np.double)
-    print("before transpose", G)
-    #G = G.transpose()
     G = matrix(G)
-    print("after transpose", G)
 
     emptyVectorN = np.zeros((n, 1)).astype(int)
     negativeY = np.negative(y)
-
 
     h = np.concatenate((emptyVectorN, y), axis=0)
     h = np.concatenate((h, negativeY), axis=0)
     h = h.astype(np.double)
     h = h.flatten()
     h = matrix(h)
-    print(h)
 
     firstHalfOfC = np.zeros(d)
     secondHathOfC = np.ones(n)
 
     c = np.concatenate((firstHalfOfC, secondHathOfC))
     c = matrix(c)
-    print(c)
 
     w = solvers.lp(c, G, h)
     return w
@@ -60,28 +57,52 @@ def minimizeL1(X, y):
 def minimizeLinf(X, y):
     n = len(X)
     d = len(X[0])
+    negativeIdentityVectorNRow = np.ones(n)
+    negativeIdentityVectorNCol = negativeIdentityVectorNRow.reshape((n,1))
+    negativeIdentityVectorN = negativeIdentityVectorNCol.astype(int)
+    emptyMatrixNxD = np.zeros((n, d)).astype(int)
+    negativeX = np.negative(X)
 
-from cvxopt import matrix, solvers
+    GFirstRow = np.concatenate((emptyMatrixNxD, negativeIdentityVectorN), axis=1)
+    GSecondRow = np.concatenate((X, negativeIdentityVectorN), axis=1)
+    GThirdRow = np.concatenate((negativeX, negativeIdentityVectorN), axis=1)
 
-A = matrix([[0., 0., 1., 4., -1., -4.],[0., 0., 2., 5., -2., -5.], [0., 0., 3., 6., -3., -6.], [-1., 0., -1., 0., -1., 0.],[0., -1., 0., -1., 0., -1.]])
-b = matrix([0.0, 0.0, 3.0, 5.0, -3.0, -5.0])
-c = matrix([0., 0., 0., 1., 1.])
+    G = np.concatenate((GFirstRow, GSecondRow), axis=0)
+    G = np.concatenate((G, GThirdRow), axis=0)
+    G = G.astype(np.double)
+    G = matrix(G)
 
-print(A)
-print(b)
-print(c)
+    print(G)
 
-sol=solvers.lp(c,A,b)
-print(sol['x'])
+    emptyVectorN = np.zeros((n, 1)).astype(int)
+    negativeY = np.negative(y)
 
-X = np.array([[1,2,3],[4,5,6]])
+    h = np.concatenate((emptyVectorN, y), axis=0)
+    h = np.concatenate((h, negativeY), axis=0)
+    h = h.astype(np.double)
+    h = h.flatten()
+    h = matrix(h)
 
-y = np.array([[3],[5]])
+    print(h)
 
-w = minimizeL1(X, y)
-print(w['x'])
+    firstHalfOfC = np.zeros(d)
+    secondHathOfC = np.ones(1)
+
+    c = np.concatenate((firstHalfOfC, secondHathOfC))
+    c = matrix(c)
+
+    print(c)
+
+    w = solvers.lp(c, G, h)
+    return w
+
+#def linearRegL20bj(x, X, y):
 
 
 
-#def minimizeLinf(X, y):
+X = np.array([[5, 3, 8],[34, 32, 1], [87, 5, 9]])
 
+y = np.array([[3],[5], [8]])
+
+w = minimizeLinf(X, y)
+print(w)
