a0edbf9caa7921aca40e8f0b5c286bba887c17ae
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-09-22 23:48:10 -0400

    worked on 2a), 2b), 2c) and added help functions too

--- A1codes.py
+++ A1codes.py
@@ -4,10 +4,29 @@ import cvxopt.solvers as solvers
 from cvxopt import matrix;
 import math
 from matplotlib import pyplot as plt
+#from jax import grad
 
 w = np.array([[3], [4]])
 X = np.array([[4, 1], [3, 3], [7, 8]])
 y = np.array([[6], [13], [23]])
+
+def L2Norm(vector):
+    sum = 0
+    for counter in range(len(vector)):
+        sum = sum + math.pow(vector[counter], 2)
+    return sum
+
+def sigmoid(vector):
+    for counter in range(len(vector)):
+        vector[counter] = 1 / (1 + np.exp(-1 * vector[counter]))
+
+    return vector
+
+def sigmoid_derivative(vector):
+    for counter in range(len(vector)):
+        vector[counter] = np.exp(-1 * vector) / math.pow(1 + np.exp(-1 * vector), 2)
+
+    return vector
 
 def minimizeL2(X, y):
     print("Starting L2")
@@ -97,9 +116,46 @@ def minimizeLinf(X, y):
     w = solvers.lp(c, G, h)
     return w
 
-#def linearRegL20bj(x, X, y):
+def linearRegL20bj(w, X, y):
+    n = len(X)
 
+    a = X*w + y
+    total = 0
 
+    for counter in range(len(a)):
+        total = total + math.pow(a[counter], 2)
+
+    obj_func = total/(2*n)
+
+    grad = (X * a)/ n
+
+    return obj_func, grad
+
+def gb(obj_func, w_init, X, y, eta, max_iter, tol):
+
+    w = w_init
+
+    for counter in range(max_iter):
+        w = linearRegL20bj(w, X, y)
+        if (L2Norm(w) < tol):
+            break
+        w = w - eta * linearRegL20bj(w, X, y)
+    return w
+
+def logisticRegObj(w, X, y):
+    n = len(X[0])
+
+    leftPart = -1 * y * np.log(sigmoid(X * w))
+    rightPart = -1 * (1 - y) * np.log(1 - sigmoid(X * w))
+
+    obj_value = (leftPart + rightPart)/n
+
+    topPart = -1 * (y/sigmoid(X * w)) * sigmoid_derivative(X * w) - (1 - y)/(1 - sigmoid(X * w)) * sigmoid_derivative(X * w)
+    bottomPart = n * (leftPart + rightPart)
+
+    grad = topPart/bottomPart
+
+    return obj_value, grad
 
 X = np.array([[5, 3, 8],[34, 32, 1], [87, 5, 9]])
 
@@ -108,8 +164,10 @@ y = np.array([[3],[5], [8]])
 w = minimizeLinf(X, y)
 print(w)
 
+out_num = np.logaddexp(0, 1)
 
-
+print(out_num)
+print(np.log(np.exp(0) + np.exp(1)))
 def loadData(dataset_folder, dataset_name):
     #auto-mpg remove origin and car name columns, and any rows with missing data, mpg is y, rest is X
     #parkinsons use status as y, rest as X
